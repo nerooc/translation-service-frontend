142e7ae5a56391d4d5fad88238a9c2864a08653f
Adrian Furman <fadikk367@gmail.com>
2022-12-21 00:16:47 +0100

    Add temporary logic in LanguagePage for MSW testing

--- src/pages/LanguagePage.tsx
+++ src/pages/LanguagePage.tsx
@@ -1,5 +1,41 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createLanguage, deleteLanguage, fetchLanguages } from "api/languages";
+import { Language, LanguageCreateData } from "api/types";
+
 type LanguagePageProps = {};
 
 export const LanguagePage = (props: LanguagePageProps) => {
-  return <div>Languages</div>;
+  const queryClient = useQueryClient();
+  const {data} = useQuery({queryKey: ['languages'], queryFn: fetchLanguages});
+  const createMutation = useMutation<Language, unknown, LanguageCreateData>({
+    mutationFn: languageData => createLanguage(languageData),
+    onSuccess: () => queryClient.invalidateQueries(['languages']),
+  });
+  const deleteMutation = useMutation<Language, unknown, number>({
+    mutationFn: id => deleteLanguage(id),
+    onSuccess: () => queryClient.invalidateQueries(['languages']),
+  });
+
+  const handleCreateLanguage = async () => {
+    createMutation.mutate({name: 'Polskaaa', code: 'PL'});
+  };
+
+  const handleDeleteLanguage = async (id: number) => {
+    deleteMutation.mutate(id);
+  };
+
+  return (
+    <div>
+      <h1>Languages</h1>
+      <ul>
+        {data?.map(language => (
+          <li key={language.id}>
+            {language.name} ({language.code})
+            <button onClick={() => handleDeleteLanguage(language.id)}>delete x</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleCreateLanguage}>Add new</button>
+    </div>
+  );
 };
