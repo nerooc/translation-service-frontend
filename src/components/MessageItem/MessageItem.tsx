c2347a94b99d3ad0367889b6597eed8cc048bdfe
Adrian Furman <fadikk367@gmail.com>
2023-01-24 14:16:04 +0100

    Refactor MessageItem styling

--- src/components/MessageItem/MessageItem.tsx
+++ src/components/MessageItem/MessageItem.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import TableCell from '@mui/material/TableCell';
-import TableRow from '@mui/material/TableRow';
 import IconButton from "@mui/material/IconButton";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -9,6 +7,7 @@ import Flag from 'react-world-flags';
 import Chip from '@mui/material/Chip';
 
 import type {MessageItemProps} from './types';
+import { ItemContainer, Cell, ButtonsContainer } from './styles';
 
 export const MessageItem = ({
   id,
@@ -22,21 +21,20 @@ export const MessageItem = ({
   const [isHovered, setIsHovered] = React.useState(false);
 
   return (
-    <TableRow
+    <ItemContainer
       key={id}
-      sx={{ display: 'flex', '&:last-child td, &:last-child th': { border: 0 }}}
       hover={isHovered}
       onMouseOver={() => setIsHovered(true)}
       onMouseOut={() => setIsHovered(false)}
     >
-      <TableCell sx={{flex: 5}}>{content}</TableCell>
-      <TableCell align="left" sx={{width: 60}}>
+      <Cell flex={5}>{content}</Cell>
+      <Cell align="left" width={60}>
         <Flag code={language.code} height={18} />
-      </TableCell>
-      <TableCell align="left" sx={{width: 60}}>
+      </Cell>
+      <Cell align="left" width={60}>
         {language.code.toUpperCase()}
-      </TableCell>
-      <TableCell sx={{flex: 2}}>
+      </Cell>
+      <Cell flex={2}>
         <Stack flexDirection="row" flexWrap="wrap" gap={1}>
           {tags.map(tag => (
             <Chip
@@ -46,17 +44,17 @@ export const MessageItem = ({
             />
           ))}
         </Stack>
-      </TableCell>
-      <TableCell align="right">
-        <Stack flexDirection="row" sx={{width: 70, opacity: isHovered ? 1 : 0, transition: 'opacity 0.2s ease-in-out'}}>
+      </Cell>
+      <Cell align="right">
+        <ButtonsContainer hovered={isHovered}>
           <IconButton size='small'>
             <EditIcon onClick={() => null} />
           </IconButton>
           <IconButton size="small">
             <DeleteIcon onClick={onDelete} />
           </IconButton>
-        </Stack>
-      </TableCell>
-    </TableRow>
+        </ButtonsContainer>
+      </Cell>
+    </ItemContainer>
   );
 }
