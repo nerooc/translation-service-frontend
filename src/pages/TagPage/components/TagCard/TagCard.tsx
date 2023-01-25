cc664a28aebe03d0fba8e4adaf9f5b339f67c40a
nerooc <tomasz_gajda@outlook.com>
2023-01-25 10:22:42 +0100

    Add reset filters button to the filters modal

--- src/pages/TagPage/components/TagCard/TagCard.tsx
+++ src/pages/TagPage/components/TagCard/TagCard.tsx
@@ -1,23 +1,22 @@
-import {useState} from "react";
-import IconButton from '@mui/material/IconButton';
-import EditIcon from '@mui/icons-material/EditOutlined';
-import DeleteIcon from '@mui/icons-material/DeleteOutlined';
-import {useMutation, useQueryClient} from "@tanstack/react-query";
+import { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/EditOutlined";
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-import {Tag} from "api/types";
-import {TagCardProps} from "./types";
-import {deleteTag} from "api/tags";
-import {EditTagModal} from "../EditTagModal";
-import {MainSection, StyledListItem, SettingsSection} from "./styles";
+import { Tag } from "api/types";
+import { TagCardProps } from "./types";
+import { deleteTag } from "api/tags";
+import { EditTagModal } from "../EditTagModal";
+import { MainSection, StyledListItem, SettingsSection } from "./styles";
 
-
-export const TagCard = ({tagData}: TagCardProps) => {
+export const TagCard = ({ tagData }: TagCardProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const queryClient = useQueryClient();
 
   const deleteMutation = useMutation<Tag, unknown, number>({
-    mutationFn: id => deleteTag(id),
-    onSuccess: () => queryClient.invalidateQueries(['tags']),
+    mutationFn: (id) => deleteTag(id),
+    onSuccess: () => queryClient.invalidateQueries(["tags"]),
   });
 
   const handleDeleteTag = (id: number) => {
@@ -26,19 +25,20 @@ export const TagCard = ({tagData}: TagCardProps) => {
 
   return (
     <StyledListItem key={tagData.id}>
-      <MainSection>
-        {tagData.name}
-      </MainSection>
+      <MainSection>{tagData.name}</MainSection>
       <SettingsSection>
-        <IconButton aria-label="delete" onClick={() => handleDeleteTag(tagData.id)}>
-          <DeleteIcon fontSize="inherit"/>
+        <IconButton
+          aria-label="delete"
+          onClick={() => handleDeleteTag(tagData.id)}
+        >
+          <DeleteIcon fontSize="inherit" />
         </IconButton>
         <IconButton aria-label="edit" onClick={() => setIsOpen(true)}>
-          <EditIcon fontSize="inherit"/>
+          <EditIcon fontSize="inherit" />
         </IconButton>
       </SettingsSection>
 
-      <EditTagModal isOpen={isOpen} setIsOpen={setIsOpen} tagData={tagData}/>
+      <EditTagModal isOpen={isOpen} setIsOpen={setIsOpen} tagData={tagData} />
     </StyledListItem>
-  )
+  );
 };
